5bed758a25bd2c7fb2639226210f0eb1f4d6dfcc
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-03-26 12:02:36 +0800

    unique_id logic.

--- coroutine-win.c
+++ coroutine-win.c
@@ -11,32 +11,50 @@
 struct coroutine;
 
 struct schedule {
-    void *fiber_ctx;
+    void *fiber;
     int cap;
-    int running;
+    int running_index;
     struct coroutine **co;
+    int unique_id;
 };
 
 struct coroutine {
     coroutine_func func;
     void *ud;
-    void *fiber_ctx;
+    void *fiber;
     struct schedule * sch;
     int index;
+    int unique_id;
     int status;
 };
+
+int generate_id(struct schedule *sch) {
+    return ++sch->unique_id;
+}
+
+struct coroutine * get_coroutine_from_id(struct schedule *sch, int id) {
+    int index;
+    struct coroutine *co;
+    for (index = 0; index < sch->cap; ++index) {
+        struct coroutine *co = sch->co[index];
+        if (co && co->unique_id == id) {
+            return co;
+        }
+    }
+    return NULL;
+}
 
 struct schedule * coroutine_open(void) {
     struct schedule *sch = (struct schedule *) calloc(1, sizeof(*sch));
     sch->cap = DEFAULT_COROUTINE;
-    sch->running = -1;
+    sch->running_index = -1;
     sch->co = (struct coroutine**) calloc(sch->cap, sizeof(struct coroutine *));
-    sch->fiber_ctx = ConvertThreadToFiber(NULL);
+    sch->fiber = ConvertThreadToFiber(NULL);
     return sch;
 }
 
 void _co_delete(struct coroutine *co) {
-    DeleteFiber(co->fiber_ctx);
+    DeleteFiber(co->fiber);
     free(co);
 }
 
@@ -47,8 +65,8 @@ void __stdcall fiber_func(void *p) {
     assert( (0 <= index) && (index < sch->cap) );
     co->func(sch, co->ud);
     co->status = COROUTINE_DEAD; // Just mark it dead, can't delete it.
-    sch->running = -1;
-    SwitchToFiber(sch->fiber_ctx);
+    sch->running_index = -1;
+    SwitchToFiber(sch->fiber);
 }
 
 struct coroutine * _co_new(struct schedule *sch, int index, coroutine_func func, void *ud) {
@@ -57,8 +75,9 @@ struct coroutine * _co_new(struct schedule *sch, int index, coroutine_func func,
     co->ud = ud;
     co->sch = sch;
     co->index = index;
+    co->unique_id = generate_id(sch);
     co->status = COROUTINE_READY;
-    co->fiber_ctx = CreateFiber(0, fiber_func, co);
+    co->fiber = CreateFiber(0, fiber_func, co);
     return co;
 }
 
@@ -76,38 +95,45 @@ void coroutine_close(struct schedule *sch) {
 }
 
 int coroutine_new(struct schedule *sch, coroutine_func func, void *ud) {
-    int id;
-    for (id = 0; id < sch->cap; ++id) {
-        struct coroutine *co = sch->co[id];
+    int index;
+    struct coroutine *co;
+    for (index = 0; index < sch->cap; ++index) {
+        struct coroutine *co = sch->co[index];
         if (co == NULL || co->status == COROUTINE_DEAD) {
             if (co && co->status == COROUTINE_DEAD) {
                 _co_delete(co);
             }
-            sch->co[id] = _co_new(sch, id, func, ud);
-            return id;
+            co = _co_new(sch, index, func, ud);
+            sch->co[index] = co;
+            return co->unique_id;
         }
     }
     sch->co = (struct coroutine **) realloc(sch->co, sch->cap * 2 * sizeof(struct coroutine *));
     assert(sch->co);
     memset(sch->co + sch->cap , 0, sizeof(struct coroutine *) * sch->cap);
-    sch->co[id] = _co_new(sch, id, func, ud);
+    co = _co_new(sch, index, func, ud);
+    sch->co[index] = co;
     sch->cap *= 2;
-    return id;
+    return co->unique_id;
 }
 
 void coroutine_resume(struct schedule *sch, int id) {
-    struct coroutine *co;
-    assert(sch->running == -1);
-    assert(id >=0 && id < sch->cap);
-    if ((co = sch->co[id]) == NULL) {
+    struct coroutine *co = get_coroutine_from_id(sch, id);
+    int index;
+    if (co == NULL) {
+        assert(!"something went wrong!");
         return;
     }
+    index = co->index;
+    assert(sch->running_index == -1);
+    assert(0 <= index && index < sch->cap);
+    assert(co == sch->co[index]);
     switch(co->status) {
     case COROUTINE_READY:
     case COROUTINE_SUSPEND:
-        sch->running = id;
+        sch->running_index = index;
         co->status = COROUTINE_RUNNING;
-        SwitchToFiber(co->fiber_ctx);
+        SwitchToFiber(co->fiber);
         break;
     default:
         assert(0);
@@ -115,23 +141,28 @@ void coroutine_resume(struct schedule *sch, int id) {
 }
 
 int coroutine_status(struct schedule *sch, int id) {
-    assert((id >= 0) && (id < sch->cap));
-    if (sch->co[id] == NULL) {
+    struct coroutine *co = get_coroutine_from_id(sch, id);
+    int index;
+    if (co == NULL) {
+        assert(!"something went wrong!");
         return COROUTINE_DEAD;
     }
-    return sch->co[id]->status;
+    index = co->index;
+    assert((0 <= index) && (index < sch->cap));
+    return co->status;
 }
 
 int coroutine_running(struct schedule *sch) {
-    return sch->running;
+    int index = sch->running_index;
+    return index >= 0 ? sch->co[index]->unique_id : -1;
 }
 
 void coroutine_yield(struct schedule *sch) {
-    int id = sch->running;
-    assert(id >= 0);
-    sch->co[id]->status = COROUTINE_SUSPEND;
-    sch->running = -1;
-    SwitchToFiber(sch->fiber_ctx);
+    int index = sch->running_index;
+    assert(index >= 0);
+    sch->co[index]->status = COROUTINE_SUSPEND;
+    sch->running_index = -1;
+    SwitchToFiber(sch->fiber);
 }
 
 #endif /* WIN32 */
